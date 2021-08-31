1cc1b280ac0486cc7093ac54b9149a40e4833b4f
PutraNur <64538855+PutraNur@users.noreply.github.com>
2021-08-31 12:00:51 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"The Putra's Bot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"The Putra's-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
